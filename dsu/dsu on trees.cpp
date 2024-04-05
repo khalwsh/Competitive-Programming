4267d666f704d51b69a00c0f401fffe6d681cb2a
unknown <khaledlabeb552@gmail.com>
2024-04-05 04:46:56 +0200

    ternary search added

--- dsu/dsu on trees.cpp
+++ dsu/dsu on trees.cpp
@@ -1,21 +1,54 @@
-int n;
-int id[N],ans[N];
+int arr[N],siz[N],res[N];
 vector<int>adj[N];
-set<int>sets[N];
-int Merge(int a,int b){
-    if(sets[a].size() > sets[b].size())swap(a,b);//small to large trick
-    for(auto &val:sets[a]){
-        sets[b].insert(val);
-    }
-    sets[a].clear();
-    return b;
-}
-int dfs(int node,int par){
-    int cur = id[node];
+vector<int>v[N];
+map<int,int>s;
+void S(int node,int par){
+    siz[node] = 1;
     for(auto &val:adj[node]){
-        if(val==par)continue;
-        cur = Merge(cur,dfs(val,node));
+        if(val == par)continue;
+        S(val,node);
+        siz[node]+=siz[val];
     }
-    ans[node] = (int)sets[cur].size();
-    return cur;
+}
+void dfs(int node,int par,bool keep) {
+    int Max = -1, BigChild = -1;
+    //find the biggest child
+    for (auto &val: adj[node]) {
+        if (Max <= siz[val] && val != par) {
+            Max = siz[val];
+            BigChild = val;
+        }
+    }
+    //make dfs calls for light children and mark them that you should erase your answer to not ruin the global answer
+    for (auto &val: adj[node]) {
+        if (val != BigChild && val != par) {
+            dfs(val, node, false);
+        }
+    }
+    //if it's not a leaf node do dfs for big child and don't erase it's content
+    if (~BigChild) {
+        dfs(BigChild, node, true);
+        swap(v[node], v[BigChild]);//O(1) operation that make cur node have the big child content to append
+    }
+    //adding my self as part of my sub tree and adding my self to the answer
+    v[node].emplace_back(node);
+    s[arr[node]]++;
+    //iterate on small children subtrees and update the answer
+    for (auto &val: adj[node]) {
+        if (val != par && val != BigChild) {
+            for(auto &x:v[val]) {
+                v[node].emplace_back(x);
+                s[arr[x]]++;
+            }
+        }
+    }
+    //calculating the answer for node
+    res[node] = (int) s.size();
+    //if you are light child erase your effect on the shared array that help me to answer to the nodes
+    if (!keep) {
+        for (auto &val: v[node]) {
+            s[arr[val]]--;
+            if (s[arr[val]] == 0)s.erase(arr[val]);
+        }
+    }
 }
