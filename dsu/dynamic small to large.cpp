e30662cdfd392686456cfef538ff9237bb406028
unknown <khaledlabeb552@gmail.com>
2024-04-06 22:40:49 +0200

    modified small to large

--- dsu/dynamic small to large.cpp
+++ dsu/dynamic small to large.cpp
@@ -1,31 +1,25 @@
 int n;
-int id[N],ans[N],values[N];
+int id[N],values[N];
 vector<int>adj[N];
-set<int>sets[N];
-int Merge(int a,int b){
-    if(sets[a].size() > sets[b].size())swap(a,b);
-    for(auto &val:sets[a]){
-        sets[b].insert(val);
-    }
-    sets[a].clear();
-    return b;
-}
+vector<int>sets[N];
+int res = 0;
 int dfs(int node,int par){
-    int cur = id[node];
+    int a = id[node];
     for(auto &val:adj[node]){
         if(val==par)continue;
-        cur = Merge(cur,dfs(val,node));
+        int b = dfs(val,node);
+        if(sets[a].size() > sets[b].size())
+            swap(a,b);
+        for(auto &x:sets[a]){
+            //small set append it to set[b]
+        }
+        sets[a].clear();
     }
-    ans[node] = (int)sets[cur].size();
-    return cur;
+    return a;
 }
 void init(){
     for(int i=0;i<n;i++){
-        sets[i].clear();
-        adj[i].clear();
-    }
-    for(int i=0;i<n;i++){
-        sets[i].insert(values[i]);
+        sets[i][values[i]]++;
         id[i] = i;
     }
 }
