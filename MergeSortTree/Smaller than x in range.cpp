04a127f778d6089e6788a0d33944fa6f827fffc1
unknown <khaledlabeb552@gmail.com>
2024-05-18 20:26:53 +0300

    Merge sort tree updated

--- MergeSortTree/Smaller than x in range.cpp
+++ MergeSortTree/Smaller than x in range.cpp
@@ -1,22 +1,25 @@
 struct MergeSortTree{
     vector<vector<int>>tree;
-    MergeSortTree(int _n){
-        tree.resize(4*_n);
+    MergeSortTree(int n){
+        tree.resize(4 * n);
     }
-    void build(int node,int nl,int nr){
-        if(nl==nr){
-            tree[node].emplace_back(Label[LinearTree[nl]]);
+    void build(int node , int nl,int nr , vector<int>&a){
+        if(nl == nr){
+            tree[node].emplace_back(a[nl]);
             return;
         }
-        int mid = nl + (nr - nl)/2;
-        build(2*node+1,nl,mid);
-        build(2*node+2,mid+1,nr);
-        merge(tree[2*node+1].begin(),tree[2*node+1].end(),tree[2*node+2].begin(),tree[2*node+2].end(), inserter(tree[node],tree[node].end()));
+        int mid = nl + (nr - nl) / 2;
+        build(2 * node + 1 , nl , mid , a);
+        build(2 * node + 2 , mid + 1 , nr , a);
+        merge(tree[2 * node + 1].begin(), tree[2 * node + 1].end(),tree[2 * node + 2].begin(), tree[2 * node + 2].end(),back_inserter(tree[node]));
     }
-    int Query(int node,int nl,int nr,int l,int r,int k){
-        if(nl>r||nr<l)return 0;
-        if(nl>=l && nr<=r)return upper_bound(tree[node].begin(),tree[node].end(),k) - tree[node].begin()  ;
-        int mid = nl + (nr - nl)/2;
-        return Query(2*node+1,nl,mid,l,r,k) + Query(2*node+2,mid+1,nr,l,r,k);
+    int query(int node,int nl,int nr,int l ,int r,int value){
+        // less than
+        if(nl >=l && nr <= r){
+            return (int)(lower_bound(tree[node].begin() , tree[node].end() , value) - tree[node].begin());
+        }
+        if(nl > r || nr < l)return 0;
+        int mid = nl + (nr - nl) / 2;
+        return query(2 * node + 1 , nl , mid , l , r ,value) + query(2 * node + 2 , mid + 1 , nr , l , r , value);
     }
 };
