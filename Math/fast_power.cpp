137aa7c2add0a85af67bfa7d28ad647a953f9bcc
khalwsh <khaledlabeb552@gamil.com>
2024-10-01 17:10:25 +0300

    centriod added

---
+++ Math/fast_power.cpp
@@ -0,0 +1,11 @@
+long long binpow(long long a, long long b, long long m) {
+    a %= m;
+    long long res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % m;
+        a = a * a % m;
+        b >>= 1;
+    }
+    return res;
+}
