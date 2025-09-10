5d76ce9b2b5e87b9e1467d78bda20d19b22f7815
khalwsh <khaledlabeb552@gmail.com>
2025-09-10 21:48:16 +0300

    treap

--- data structure/treap/treap with parent pointer.cpp
+++ data structure/treap/treap with parent pointer.cpp
@@ -4,14 +4,19 @@ long long randomLongLong(long long l, long long r) {
     uniform_int_distribution<long long> dist(l, r);
     return dist(rng);
 }
+// init id every test , the nodes return from split make it leader of set
+int id = 1;
 struct Treap {
     ll val , priority , size;
     Treap *kids[2];
     Treap *parent;
-    Treap(int k = 0) {
-        val = k;
+    ll Id;
+    ll sum = 0;
+    Treap(ll k = 0) {
+        val = sum = k;
         size = 1;
         parent = this;
+        Id = id++;
         priority = randomLongLong(1 , 1e18);
         kids[0] = kids[1] = nullptr;
     }
@@ -23,7 +28,8 @@ struct Treap {
 void recalc(Treap* me) {
     if (me == nullptr) return;
     me -> size = 1;
-    for (auto child : me->kids) if (child) me -> size += child -> size;
+    me -> sum = me -> val;
+    for (auto child : me->kids) if (child) me -> size += child -> size , me -> sum += child -> sum;
 }
 ll Size(Treap *me) {
     if (me == nullptr) return  0;
@@ -38,8 +44,8 @@ pair<Treap*, Treap*> split(Treap* me, int toLeft) {
         if (me->kids[0]) me->kids[0]->parent = me;
         recalc(me);
 
-        if (p.first) p.first->parent = nullptr; 
-        me->parent = nullptr;                   
+        if (p.first) p.first->parent = p.first;
+        me->parent = me;
         return {p.first, me};
     } else {
         auto p = split(me->kids[1], toLeft - Size(me->kids[0]) - 1);
@@ -47,19 +53,19 @@ pair<Treap*, Treap*> split(Treap* me, int toLeft) {
         if (me->kids[1]) me->kids[1]->parent = me;
         recalc(me);
 
-        if (p.second) p.second->parent = nullptr; 
-        me->parent = nullptr;                    
+        if (p.second) p.second->parent = p.second;
+        me->parent = me;
         return {me, p.second};
     }
 }
 
 Treap* Merge(Treap* left, Treap* right) {
     if (left == nullptr) {
-        if (right) right->parent = nullptr;
+        if (right) right->parent = right;
         return right;
     }
     if (right == nullptr) {
-        left->parent = nullptr;
+        left->parent = left;
         return left;
     }
 
@@ -67,13 +73,21 @@ Treap* Merge(Treap* left, Treap* right) {
         left->kids[1] = Merge(left->kids[1], right);
         if (left->kids[1]) left->kids[1]->parent = left;
         recalc(left);
-        left->parent = nullptr; 
+        left->parent = left;
         return left;
     } else {
         right->kids[0] = Merge(left, right->kids[0]);
         if (right->kids[0]) right->kids[0]->parent = right;
         recalc(right);
-        right->parent = nullptr;
+        right->parent = right;
         return right;
+    }
+}
+Treap* get_parent(Treap* me) {
+    if (me == nullptr) return nullptr;
+    while (true) {
+        assert(me -> parent);
+        if (me -> parent -> Id == me -> Id) return me;
+        me = me -> parent;
     }
 }
