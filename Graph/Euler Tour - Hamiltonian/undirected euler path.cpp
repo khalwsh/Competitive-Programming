d23642da8ffc19114570a7a5ecf2ccc522a25902
khalwsh <khaledlabeb552@gmail.com>
2025-11-28 22:07:24 +0200

    all

--- Graph/Euler Tour - Hamiltonian/undirected euler path.cpp
+++ Graph/Euler Tour - Hamiltonian/undirected euler path.cpp
@@ -1,49 +1,73 @@
 const int N = 2e5 + 10;
 int n, m;
-set<int> adj[N];
+multiset<int> adj[N];
 int deg[N];
 vector<int> tour;
 
-void dfs(int node){
-    while(!adj[node].empty()){
-        auto it = adj[node].begin();
-        int x = *it;
-        adj[node].erase(it);
-        if(adj[x].erase(node)){
-            dfs(x);
+void dfs(int u) {
+    while (!adj[u].empty()) {
+        auto it = adj[u].begin();
+        int v = *it;
+        adj[u].erase(it);
+        if (u != v) {
+            auto it_rev = adj[v].find(u);
+            if (it_rev != adj[v].end()) {
+                adj[v].erase(it_rev);
+            }
         }
+        dfs(v);
     }
-    tour.emplace_back(node);
+    tour.emplace_back(u);
 }
 
-bool check(){
-    int odd = 0;
-    for(int i = 0; i < n; ++i){
-        if(deg[i] & 1) ++odd;
+bool check() {
+    for (int i = 0; i < n; ++i) {
+        if (deg[i] & 1) return false;
     }
-    return (odd == 0 || odd == 2);
+    return true;
 }
 
-bool get(){
-    if(!check()) return false;
+bool isConnected() {
     int start = -1;
-    for(int i = 0; i < n; ++i){
-        if(deg[i] & 1){ start = i; break; }
+    for (int i = 0; i < n; ++i) {
+        if (deg[i] > 0) { start = i; break; }
     }
-    if(start == -1){
-        for(int i = 0; i < n; ++i){
-            if(deg[i] > 0){ start = i; break; }
+    if (start == -1) return true;
+
+    vector<char> vis(n, 0);
+    stack<int> st;
+    st.push(start);
+    vis[start] = 1;
+    while (!st.empty()) {
+        int v = st.top(); st.pop();
+        for (int u : adj[v]) {
+            if (!vis[u]) {
+                vis[u] = 1;
+                st.push(u);
+            }
         }
     }
 
-    if(start == -1){
+    for (int i = 0; i < n; ++i) {
+        if (deg[i] > 0 && !vis[i]) return false;
+    }
+    return true;
+}
+
+bool get() {
+    if (!check()) return false;
+    if (!isConnected()) return false;
+
+    int start = -1;
+    for (int i = 0; i < n; ++i) {
+        if (deg[i] > 0) { start = i; break; }
+    }
+    if (start == -1) {
         return (m == 0);
     }
 
     tour.clear();
     dfs(start);
     reverse(tour.begin(), tour.end());
-
-    if((int)tour.size() != m + 1) return false;
-    return true;
+    return (int)tour.size() == m + 1;
 }
