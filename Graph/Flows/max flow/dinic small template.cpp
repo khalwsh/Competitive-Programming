5dc400ae765b38d962f13e9c03cd5b5bc3f69c2a
khalwsh <khaledlabeb552@gmail.com>
2026-02-06 13:47:42 +0200

    last version

--- Graph/Flows/max flow/dinic small template.cpp
+++ Graph/Flows/max flow/dinic small template.cpp
@@ -1,52 +1,69 @@
+template <typename T>
 struct Dinic {
     struct Edge {
         int to, rev;
-        ll c, oc;
-        ll flow() { return max(oc - c, 0LL); } // if you need flows
+        T c, oc;
+        T flow() const { return oc - c; } // flow through edge
     };
 
     vector<int> lvl, ptr, q;
-    vector<vector<Edge> > adj;
+    vector<vector<Edge>> adj;
 
-    Dinic(int n) : lvl(n), ptr(n), q(n), adj(n) {
+    Dinic(int n) : lvl(n), ptr(n), q(n), adj(n) {}
+
+    void addEdge(int a, int b, T c, T rcap = 0) {
+        adj[a].push_back({b, (int)adj[b].size(), c, c});
+        adj[b].push_back({a, (int)adj[a].size() - 1, rcap, rcap});
     }
 
-    void addEdge(int a, int b, ll c, ll rcap = 0) {
-        adj[a].push_back({b, (int) adj[b].size(), c, c});
-        adj[b].push_back({a, (int) adj[a].size() - 1, rcap, rcap});
-    }
-
-    ll dfs(int v, int t, ll f) {
-        if (v == t || !f) return f;
-        for (int &i = ptr[v]; i < (int) adj[v].size(); i++) {
+    T dfs(int v, int t, T f) {
+        if (v == t || f == 0) return f;
+        for (int &i = ptr[v]; i < (int)adj[v].size(); i++) {
             Edge &e = adj[v][i];
-            if (lvl[e.to] == lvl[v] + 1)
-                if (ll p = dfs(e.to, t, min(f, e.c))) {
-                    e.c -= p, adj[e.to][e.rev].c += p;
-                    return p;
+            if (lvl[e.to] == lvl[v] + 1 && e.c > 0) {
+                T pushed = dfs(e.to, t, min(f, e.c));
+                if (pushed) {
+                    e.c -= pushed;
+                    adj[e.to][e.rev].c += pushed;
+                    return pushed;
                 }
+            }
         }
         return 0;
     }
 
-    ll calc(int s, int t) {
-        ll flow = 0;
+    T calc(int s, int t) {
+        T flow = 0;
         q[0] = s;
-        for (int L = 0; L <= 31; L++)
+
+        for (int L = 0; L <= 31; L++) {
             do {
-                // 'int L=30' maybe faster for random data
-                lvl = ptr = vector<int>((int) q.size());
-                int qi = 0, qe = lvl[s] = 1;
+                fill(lvl.begin(), lvl.end(), 0);
+                fill(ptr.begin(), ptr.end(), 0);
+
+                int qi = 0, qe = 0;
+                q[qe++] = s;
+                lvl[s] = 1;
+
                 while (qi < qe && !lvl[t]) {
                     int v = q[qi++];
-                    for (Edge e: adj[v])
-                        if (!lvl[e.to] && e.c >> (30 - L))
-                            q[qe++] = e.to, lvl[e.to] = lvl[v] + 1;
+                    for (const Edge &e : adj[v]) {
+                        if (!lvl[e.to] && (e.c >> (30 - L)) > 0) {
+                            lvl[e.to] = lvl[v] + 1;
+                            q[qe++] = e.to;
+                        }
+                    }
                 }
-                while (ll p = dfs(s, t, LLONG_MAX)) flow += p;
+
+                while (T pushed = dfs(s, t, numeric_limits<T>::max()))
+                    flow += pushed;
+
             } while (lvl[t]);
+        }
         return flow;
     }
 
-    bool leftOfMinCut(int a) { return lvl[a] != 0; }
+    bool leftOfMinCut(int a) const {
+        return lvl[a] != 0;
+    }
 };
