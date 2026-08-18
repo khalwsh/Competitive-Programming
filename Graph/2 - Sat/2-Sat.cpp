29c51875dcdc4316efb4907b24666b176b84c0ec
khalwsh <khaledlabeb552@gmail.com>
2026-08-18 18:23:31 +0300

    Refactor SCC_Solver and TwoSAT classes

--- Graph/2 - Sat/2-Sat.cpp
+++ Graph/2 - Sat/2-Sat.cpp
@@ -1,78 +1,156 @@
-struct SCC_Solver {
+struct SCC {
     int n;
-    vector<vector<int> > g;
-    SCC_Solver(int n) : n(n), g(n) {
+    vector<vector<int>> g;
+
+    SCC(int n) : n(n), g(n) {}
+
+    void add(int u, int v) {
+        g[u].push_back(v);
     }
-    void add_edge(int u, int v) { g[u].push_back(v); }
-    pair<int, vector<int> > scc_ids() {
-        vector<int> tin(n, -1), low(n), id(n), stk;
-        int timer = 0, scc_cnt = 0;
-        stk.reserve(n);
+
+    pair<int, vector<int>> ids() {
+        vector<int> in(n, -1), low(n), id(n), st;
+        int timer = 0, cnt = 0;
+
         function<void(int)> dfs = [&](int v) {
-            low[v] = tin[v] = timer++;
-            stk.push_back(v);
-            for (int to: g[v]) {
-                if (tin[to] == -1) {
-                    dfs(to);
-                    low[v] = min(low[v], low[to]);
-                } else if (tin[to] < n) {
-                    // Node is on stack
-                    low[v] = min(low[v], tin[to]);
+            low[v] = in[v] = timer++;
+            st.push_back(v);
+
+            for (int u : g[v]) {
+                if (in[u] == -1) {
+                    dfs(u);
+                    low[v] = min(low[v], low[u]);
+                } else if (in[u] < n) {
+                    low[v] = min(low[v], in[u]);
                 }
             }
-            if (low[v] == tin[v]) {
+
+            if (low[v] == in[v]) {
                 while (true) {
-                    int u = stk.back();
-                    stk.pop_back();
-                    tin[u] = n; // Mark as processed
-                    id[u] = scc_cnt;
+                    int u = st.back();
+                    st.pop_back();
+                    in[u] = n;
+                    id[u] = cnt;
                     if (u == v) break;
                 }
-                scc_cnt++;
+                cnt++;
             }
         };
-        for (int i = 0; i < n; i++) if (tin[i] == -1) dfs(i);
-        for (auto &x: id) x = scc_cnt - 1 - x;
-        return {scc_cnt, id};
-    }
-    vector<vector<int> > sccs() {
-        auto p = scc_ids();
-        int scc_cnt = p.first;
-        vector<int> id = p.second;
-        vector<vector<int> > res(scc_cnt);
-        for (int i = 0; i < n; i++) res[id[i]].push_back(i);
-        return res;
+
+        for (int i = 0; i < n; i++)
+            if (in[i] == -1)
+                dfs(i);
+
+        for (int &x : id)
+            x = cnt - 1 - x;
+
+        return {cnt, id};
     }
 };
+
 struct TwoSAT {
-    int n;
+    int n, tot;
     vector<bool> ans;
-    SCC_Solver s;
-    TwoSAT(int n) : n(n), ans(n), s(2 * n) {}
-    void add_or(int i, bool f, int j, bool g) {
-        s.add_edge(2 * i + !f, 2 * j + g);
-        s.add_edge(2 * j + !g, 2 * i + f);
+    SCC s;
+    vector<int> tr, fl;
+
+    TwoSAT(int n)
+        : n(n),
+          tot(n),
+          ans(n),
+          s(2 * (9 * n + 5)),
+          tr(4 * n + 5, -1),
+          fl(4 * n + 5, -1) {
+        build(tr, 1, 0, n - 1, 1);
+        build(fl, 1, 0, n - 1, 0);
     }
+
+    int new_var() {
+        return tot++;
+    }
+
+    void add_or(int i, bool f, int j, bool g) {
+        s.add(2 * i + !f, 2 * j + g);
+        s.add(2 * j + !g, 2 * i + f);
+    }
+
+    void Imply(int i, bool f, int j, bool g) {
+        add_or(i, !f, j, g);
+    }
+
     void Xor(int i, bool f, int j, bool g) {
         add_or(i, f, j, g);
         add_or(i, !f, j, !g);
     }
-    void Imply(int i, bool f, int j, bool g) { add_or(i, !f, j, g); }
-    void BiImply(int i, bool f, int j, bool g) {
-        // XNOR
+
+    void XNOR(int i, bool f, int j, bool g) {
         Imply(i, f, j, g);
-        Imply(j, f, i, g);
+        Imply(j, g, i, f);
     }
-    void force_true(int i) { add_or(i, true, i, true); }
-    void force_false(int i) { add_or(i, false, i, false); }
+
+    void XNAND(int i, bool f, int j, bool g) {
+        add_or(i, !f, j, !g);
+    }
+
+    void AND(int i, bool f, int j, bool g) {
+        force(i, f);
+        force(j, g);
+    }
+
+    void force(int i, bool f) {
+        add_or(i, f, i, f);
+    }
+
+    void build(vector<int> &t, int p, int l, int r, bool val) {
+        t[p] = new_var();
+
+        if (l == r) {
+            Imply(t[p], 1, l, val);
+            return;
+        }
+
+        int m = (l + r) >> 1;
+
+        build(t, p << 1, l, m, val);
+        build(t, p << 1 | 1, m + 1, r, val);
+
+        Imply(t[p], 1, t[p << 1], 1);
+        Imply(t[p], 1, t[p << 1 | 1], 1);
+    }
+
+    void update(vector<int> &t, int p, int l, int r, int ql, int qr) {
+        if (qr < l || r < ql)
+            return;
+
+        if (ql <= l && r <= qr) {
+            force(t[p], 1);
+            return;
+        }
+
+        int m = (l + r) >> 1;
+
+        update(t, p << 1, l, m, ql, qr);
+        update(t, p << 1 | 1, m + 1, r, ql, qr);
+    }
+
+    void set_range_true(int l, int r) {
+        update(tr, 1, 0, n - 1, l, r);
+    }
+
+    void set_range_false(int l, int r) {
+        update(fl, 1, 0, n - 1, l, r);
+    }
+
     bool solve() {
-        auto p = s.scc_ids();
-        vector<int> id = p.second;
+        auto [cnt, id] = s.ids();
+
         for (int i = 0; i < n; i++) {
-            if (id[2 * i] == id[2 * i + 1]) return false;
+            if (id[2 * i] == id[2 * i + 1])
+                return false;
+
             ans[i] = id[2 * i] < id[2 * i + 1];
         }
+
         return true;
     }
-    vector<bool> get_ans() { return ans; }
 };
